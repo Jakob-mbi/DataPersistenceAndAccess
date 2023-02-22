ff2e638766e5f38f7269b812a823400a693e114d
Jakob-mbi <jakob_mbi@msn.com>
2023-02-22 19:29:47 +0100

    feat: Data access methods

---
+++ DataPersistenceAndAccess/DataAccess/Customers/ICustomerRepository.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using DataPersistenceAndAccess.Models;
+
+namespace DataPersistenceAndAccess.DataAccess.Customers
+{
+    public interface ICustomerRepository : ICrudRepository<Customer,int>
+    {
+        public Customer GetByName(string name);
+        /// <summary>
+        /// Retrieves a particular instance from the database by its name.
+        /// </summary>
+        /// <param /*name="name"*/></param>
+        /// <returns>Customer</returns>
+        public List<Customer> GetLimitedListWiithOffset(int limit,int offset);
+        /// <summary>
+        /// Retrieves limited list off instances with offset from the database.
+        /// </summary>
+        /// <returns>List<Customer></returns>
+        public List<CustomerCountry> GetListOfCountriesWithAmountOfCustomers();
+        /// <summary>
+        /// Retrieves list countries with amount Of customers for each country.
+        /// </summary>
+        /// <returns>List<CustomerCountry></returns>
+        public List<CustomerSpender> GetListOfHighestSpendingCustomers();
+        /// <summary>
+        /// Retrieves list of customers who are the highest spenders.
+        /// </summary>
+        /// <returns>List<CustomerCountry></returns>
+        /// 
+        public CustomerGenre GetListOfCustomerMostPopularGenre(int id);
+        /// <summary>
+        /// Retrieves most popular genre of a given customer.
+        /// </summary>
+        /// <returns>CustomerGenre</returns>
+    }
+}
---
+++ DataPersistenceAndAccess/DataAccess/ICrudRepository.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DataPersistenceAndAccess.DataAccess
+{
+    public interface ICrudRepository<T, ID>
+    {
+        
+        List<T> GetAll();
+        /// <summary>
+        /// Retrieves all instances from the database.
+        /// </summary>
+        /// <returns>List<T></returns>
+       
+        T GetById(ID id);
+        /// <summary>
+        /// Retrieves a particular instance from the database by its ID.
+        /// </summary>
+        /// <param /*name="id"*/></param>
+        /// <returns>T</returns>
+        
+        void Add(T obj);
+        /// <summary>
+        /// Inserts a new row into the database based on the paramter.
+        /// </summary>
+        /// <param /*name="obj"*/></param>
+        
+        void Update(T obj);
+        /// <summary>
+        /// Updates an existing row based on the provided parameters.
+        /// </summary>
+        /// <param/* name="obj"*/></param>
+        
+        void Delete(ID id);
+        /// <summary>
+        /// Deteles a record by its ID.
+        /// </summary>
+        /// <param /*name="id"*/></param>
+    }
+}
+
+
+
